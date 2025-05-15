7c3e54d68aac25d73afcabd9209caf45ccee2528
WebDeveloperArun <forwebdeveloper0@gmail.com>
2025-05-15 19:20:31 +0530

    created a authentication otp and send email functionality

---
+++ apps/auth-service/src/utils/auth.helper.ts
@@ -0,0 +1,75 @@
+import crypto from "crypto";
+import { ValidationError } from "../../../../packages/error-handler";
+import { NextFunction } from "express";
+import redis from "../../../../packages/libs/redis";
+import { sendEmail } from "./sendMail";
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export const validateRegistrationData = (
+  data: any,
+  userType: "user" | "seller"
+) => {
+  const { name, email, password, phone_number, country } = data;
+
+  if (
+    !name ||
+    !email ||
+    !password(userType === "seller" && (!phone_number || !country))
+  ) {
+    throw new ValidationError("Missing required fields!");
+  }
+
+  if (!emailRegex.test(email)) {
+    throw new ValidationError("Invalid email format!");
+  }
+};
+
+export const checkOtpRestrictions = async (
+  email: string,
+  next: NextFunction
+) => {
+  if (await redis.get(`otp_cooldown:${email}`)) {
+    return next(
+      new ValidationError("Please wait for 1 minute before trying again.")
+    );
+  }
+  if (await redis.get(`otp_lock:${email}`)) {
+    return next(
+      new ValidationError(
+        "Account locked due to multiple failed attempts. Try again after 30 minutes."
+      )
+    );
+  }
+  if (await redis.get(`otp_spam_lock:${email}`)) {
+    return next(
+      new ValidationError("Too many attempts. Please try again after 1 hour.")
+    );
+  }
+};
+
+export const trackOtpRequests = async (email: string, next: NextFunction) => {
+  const otpRequestkey = `otp_requests:${email}`;
+  let otpRequests = parseInt((await redis.get(otpRequestkey)) || "0");
+
+  if (otpRequests >= 2) {
+    await redis.set(`otp_spam_lock:${email}`, "locked", "EX", 3600); // lock for 1 hour
+    return next(
+      new ValidationError("Too many attempts. Please try again after 1 hour.")
+    );
+  }
+
+  await redis.set(otpRequestkey, otpRequests + 1, "EX", 3600);
+};
+
+export const sendOtp = async (
+  name: string,
+  email: string,
+  template: string
+) => {
+  const otp = crypto.randomInt(1000, 9999).toString();
+
+  await sendEmail(email, "Verify your email", template, { name, otp });
+  await redis.set(`otp:${email}`, otp, "EX", 300);
+  await redis.set(`otp_cooldown:${email}`, "true", "EX", 60);
+};
---
+++ apps/auth-service/src/utils/sendMail/index.ts
@@ -0,0 +1,39 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+import ejs from "ejs";
+import path from "path";
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT) || 587,
+  service: process.env.SMTP_SERVICE,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+// Render an Ejs email template
+const randerEmailTemplate = async (tamplateName: string, data: Record<string, any>) => {
+    const templatePath = path.join(process.cwd(), "auth-service", "src", "utils", "email-templates", `${tamplateName}.ejs`);
+
+    return ejs.renderFile(templatePath, data);
+}
+
+export const sendEmail = async (to: string, subject: string, tamplateName: string, data: Record<string, any>) => {
+    try {
+        const html = await randerEmailTemplate(tamplateName, data);
+        await transporter.sendMail({
+            from: `<${process.env.SMTP_USER}>`,
+            to,
+            subject,
+            html,
+        });
+        return true;
+    } catch (error) {
+        console.log("Error sending email", error);
+        return false;
+    }
+}
